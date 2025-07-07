d25b82c4dcba4992134f0a8b23911d37d8853791
amindm <dashtiam@amazon.com>
2025-07-07 22:48:48 +0000

    add spectrum finetunening

--- 18_sagemaker_training_recipes/ft_deepseek_r1_qlora/scripts/convert.sh
+++ 18_sagemaker_training_recipes/ft_deepseek_r1_qlora/scripts/convert.sh
@@ -9,40 +9,44 @@ echo "***** The FSx data channel path for the model is located at: $FSX_PATH"
 
 pwd
 
+sm_adapter_placeholder="sm_adapter_download"
+deepseek_inference_placeholder="deepseek_inference"
+
+mkdir $FSX_PATH/$sm_adapter_placeholder
+mkdir $FSX_PATH/$deepseek_inference_placeholder
+
+cd $FSX_PATH/$sm_adapter_placeholder
+git clone https://github.com/aws/sagemaker-hyperpod-training-adapter-for-nemo.git
+
+cd $FSX_PATH/$deepseek_inference_placeholder
 # Clone the DeepSeek-V3 repository
 git clone https://github.com/deepseek-ai/DeepSeek-V3.git
 
 # Change to the inference directory and install requirements
-cd DeepSeek-V3/inference
+cd DeepSeek-V3
+find . -type d -exec touch {}/__init__.py \;
+cd inference
 pip install -r requirements.txt
 
-echo "***** Downloading the file now"
+cp -r $FSX_PATH/$sm_adapter_placeholder/sagemaker-hyperpod-training-adapter-for-nemo/src/hyperpod_nemo_adapter/scripts/* .
 
-# Check if fp8_cast_bf16.py exists
-if [ -f fp8_cast_bf16.py ]; then
-    echo "***** Removing existing fp8_cast_bf16.py"
-    rm fp8_cast_bf16.py
-else
-    echo "***** fp8_cast_bf16.py does not exist"
-fi
+echo "***** Creating dir $FSX_PATH/$CONVERTED_DIR"
+mkdir $FSX_PATH/$CONVERTED_DIR
+ls -ltr $FSX_PATH/$CONVERTED_DIR
 
-# Download the new fp8_cast_bf16.py file
-echo "***** Downloading new fp8_cast_bf16.py"
-curl -O https://raw.githubusercontent.com/aws/sagemaker-hyperpod-training-adapter-for-nemo/main/src/hyperpod_nemo_adapter/scripts/fp8_cast_bf16.py
+echo "***** Starting conversion from FP8 to Bf16 from $FSX_PATH to $FSX_PATH/$CONVERTED_DIR"
 
-# Verify the download
-if [ -f fp8_cast_bf16.py ]; then
-    echo "***** fp8_cast_bf16.py successfully downloaded"
-    ls -ltr fp8_cast_bf16.py
-else
-    echo "***** Error: fp8_cast_bf16.py download failed"
-    exit 1
-fi
+echo "Current Dir: $(pwd)"
 
-echo "***** Starting conversion from FP8 to Bf16"
+current_inf_path=$(pwd)
 
+export PYTHONPATH=${PYTHONPATH}:${current_inf_path}
+export PYTHONPATH=${PYTHONPATH}:${current_inf_path}/casting_utils
+echo "PYTHONPATH:"$PYTHONPATH
+
+cd $FSX_PATH/$deepseek_inference_placeholder
 # Run the conversion script
-python fp8_cast_bf16.py --input-fp8-hf-path "$FSX_PATH" --output-bf16-hf-path "$FSX_PATH/$CONVERTED_DIR"
+python -m DeepSeek-V3.inference.fp8_cast_bf16 --input-fp8-hf-path "$FSX_PATH" --output-bf16-hf-path "$FSX_PATH/$CONVERTED_DIR"
 
 echo "Current working directory: $pwd"
 
@@ -52,6 +56,9 @@ find $FSX_PATH -type f \
     ! -name "*.safetensors" \
     ! -name "*metadata*" \
     ! -name "*lock*" \
+    -not -path "*/$sm_adapter_placeholder/*" \
+    -not -path "*/$deepseek_inference_placeholder/*" \
+    -not -path "*/$FSX_PATH/$CONVERTED_DIR//*" \
     -exec cp -v {} "$FSX_PATH/$CONVERTED_DIR/" \;
 
 echo "***** Script execution completed"
