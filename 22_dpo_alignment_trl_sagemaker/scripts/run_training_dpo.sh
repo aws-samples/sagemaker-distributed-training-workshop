b88de0724317d3973b4da5aa59384fbbf7ef7f37
amindm <dashtiam@amazon.com>
2025-07-16 21:51:34 +0000

    fix number gpu in run.sh files

--- 22_dpo_alignment_trl_sagemaker/scripts/run_training_dpo.sh
+++ 22_dpo_alignment_trl_sagemaker/scripts/run_training_dpo.sh
@@ -8,6 +8,10 @@ tar -xzvf /opt/ml/input/model/model.tar.gz -C /opt/ml/input/model/
 
 aws s3 cp $data_location /opt/ml/input/data/dataset/
 
+NUM_GPUS=$(nvidia-smi --list-gpus | wc -l)
+echo "Detected ${NUM_GPUS} GPUs on the machine"
+
+
 python merge_adapter_weights.py --peft_model_id /opt/ml/input/model/llama-3-2-3b-function-calling/ --output_dir /opt/ml/input/model/merged-weights --save_tokenizer True
 
-accelerate launch --config_file accelerate_configs/deepspeed_zero3.yaml --num_processes 8 run_dpo.py --config receipes/sft-dpo-llama-3-2-3b.yaml
+accelerate launch --config_file accelerate_configs/deepspeed_zero3.yaml --num_processes ${NUM_GPUS} run_dpo.py --config receipes/sft-dpo-llama-3-2-3b.yaml
--- 22_dpo_alignment_trl_sagemaker/scripts/run_training_sft.sh
+++ 22_dpo_alignment_trl_sagemaker/scripts/run_training_sft.sh
@@ -3,5 +3,7 @@
 huggingface-cli login --token $HF_token
 aws s3 cp $data_location /opt/ml/input/data/dataset/
 
+NUM_GPUS=$(nvidia-smi --list-gpus | wc -l)
+echo "Detected ${NUM_GPUS} GPUs on the machine"
 
-accelerate launch --config_file accelerate_configs/deepspeed_zero3.yaml --num_processes 8 run_sft.py --config receipes/sft-llama-3-2-3b-qlora.yaml
+accelerate launch --config_file accelerate_configs/deepspeed_zero3.yaml --num_processes ${NUM_GPUS} run_sft.py --config receipes/sft-llama-3-2-3b-qlora.yaml
